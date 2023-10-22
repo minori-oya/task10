aaa44493717ce4561846b51c7e42c78b834086ba
minori <minori99525@au.com>
2023-10-22 16:29:04 +0900

    ClientResponseクラスの各フィールドとClientServiceImplクラスの依存関係ClientMapperにfinalを追加。

--- src/main/java/com/task10/crudapi_login/service/ClientServiceImpl.java
+++ src/main/java/com/task10/crudapi_login/service/ClientServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @Service
 public class ClientServiceImpl implements ClientService {
-    private ClientMapper clientMapper;
+    private final ClientMapper clientMapper;
 
     public ClientServiceImpl(ClientMapper clientMapper) {
         this.clientMapper = clientMapper;
